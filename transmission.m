cd8dec018ccf14bad1318454bf56bd70d654be43
Ankan Dutta <40065301+ankann@users.noreply.github.com>
2018-07-13 12:21:21 +0530

    Update transmission.m

--- transmission.m
+++ transmission.m
@@ -2,7 +2,8 @@ clear variables
 numberOfVariables=3;
 [R1,fval] = gamultiobj(@simple_multiobjective,numberOfVariables,[],[],[],[],[],[],@mycon);
 disp(R1);
-for i=1:length(R1)
+[m,n]=size(R1);
+for i=1:m
     D=1; A=D./R1(i,2); C= D./R1(i,3); B= sqrt(A.^2+C.^2+D.^2-2*R1(i,1).*A.*C);
     disp([A B C D]);
     t1=B+D-A-C;
@@ -21,27 +22,23 @@ for i=1:length(R1)
         disp('Non-grashof');
     end
 end
-
-
-% when C^2+B^2 tends to D^2 and A tends to 0
-% function tr=simple_multiobjective(R)
-%     tr(1)=abs((2/R(3)^2)+(1/R(2)^2)-(2*R(1)/(R(2)*R(3))));
-%     tr(2)=abs(1/R(2));
-% end
-% function [j,jeq]=mycon(x)
-% r=0.2;
-%     j(1)=abs(((1/x(3))-(x(1)/x(2))+(x(3)/x(2)))/sqrt(1+(1/x(2)^2)+(1/x(3)^2)-(2*x(1)/(x(2)*x(3)))))-sin(r);
-%     j(1)=abs(((1/x(3))-(x(1)/x(2))-(x(3)/x(2)))/sqrt(1+(1/x(2)^2)+(1/x(3)^2)-(2*x(1)/(x(2)*x(3)))))-sin(r);
-%     jeq=[];
-% end
-
-% when C^2+B^2 tends to A^2 and D tends to zero comparetively
 function tr=simple_multiobjective(R)
-    tr(1)=abs((2/R(3)^2)+1-(2*R(1)/(R(2)*R(3))));
-    tr(2)=abs(R(2));
+    tr(1)=abs((2/R(3)^2)+(1/R(2)^2)-(2*R(1)/(R(2)*R(3))));
+    tr(2)=abs(1/R(2));
 end
-function [j,jeq]=mycon(x)    
-    j(1)=abs(((1/x(3))-(x(1)/x(2))+(x(3)/x(2)))/sqrt(1+(1/x(2)^2)+(1/x(3)^2)-(2*x(1)/(x(2)*x(3)))))-1;
-    j(2)=abs(((1/x(3))-(x(1)/x(2))-(x(3)/x(2)))/sqrt(1+(1/x(2)^2)+(1/x(3)^2)-(2*x(1)/(x(2)*x(3)))))-1;
+function [j,jeq]=mycon(x)
+r=0.2;
+    j(1)=abs(((1/x(3))-(x(1)/x(2))+(x(3)/x(2)))/sqrt(1+(1/x(2)^2)+(1/x(3)^2)-(2*x(1)/(x(2)*x(3)))))-sin(r);
+    j(1)=abs(((1/x(3))-(x(1)/x(2))-(x(3)/x(2)))/sqrt(1+(1/x(2)^2)+(1/x(3)^2)-(2*x(1)/(x(2)*x(3)))))-sin(r);
     jeq=[];
 end
+% function tr=simple_multiobjective(R)
+%     tr(1)=abs((2/R(3)^2)+1-(2*R(1)/(R(2)*R(3))));
+%     tr(2)=abs(R(2)-0.01);
+% end
+% function [j,jeq]=mycon(x)    
+%     j(1)=abs(((1/x(3))-(x(1)/x(2))+(x(3)/x(2)))/sqrt(1+(1/x(2)^2)+(1/x(3)^2)-(2*x(1)/(x(2)*x(3)))))-1;
+%     j(2)=abs(((1/x(3))-(x(1)/x(2))-(x(3)/x(2)))/sqrt(1+(1/x(2)^2)+(1/x(3)^2)-(2*x(1)/(x(2)*x(3)))))-1;
+%     j(3)=-(1/x(2)^2+1/x(3)^2+1-2*x(1)/(x(2)*x(3)));
+%     jeq=[];
+% end
